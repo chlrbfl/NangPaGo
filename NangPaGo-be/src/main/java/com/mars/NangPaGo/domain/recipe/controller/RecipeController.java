f06746417c7e06e49f1c21f666934cb1f4f8250c
chlrbfl <kelly07192@gmail.com>
2024-12-24 00:34:45 +0900

    [NPG-47] recipe-detail-api 1차 구현

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/controller/RecipeController.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/controller/RecipeController.java
@@ -1,5 +1,10 @@
 package com.mars.NangPaGo.domain.recipe.controller;
 
+import com.mars.NangPaGo.domain.recipe.entity.Recipe;
+import com.mars.NangPaGo.domain.recipe.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RecipeController {
 
-    //
+    @Autowired
+    private RecipeService recipeService;
+
+    // 레시피 목록 전체 조회
+
+    // 레시피 상세 조회
+    @GetMapping("/detail/{id}")
+    public Recipe getRecipe(@PathVariable Long id) {
+       recipeService.toString();
+    }
+
 
 
 }
--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/dto/RecipeRequestDto.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/dto/RecipeRequestDto.java
@@ -1,7 +1,21 @@
 package com.mars.NangPaGo.domain.recipe.dto;
 
+import com.mars.NangPaGo.domain.recipe.entity.Manual;
+import com.mars.NangPaGo.domain.recipe.entity.ManualImage;
+import com.mars.NangPaGo.domain.recipe.entity.Recipe;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class RecipeRequestDto {
+
+    private Recipe recipe;
+    private Manual manual;
+    private ManualImage manualImage;
+
+
+
+
 }
